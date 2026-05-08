cf4a6694b82ad88db83e65ed587f7a0951aaeecb
ViniciusLira1 <liravinicius76@gmail.com>
2026-05-08 19:49:57 -0300

    fix: aponta backend para render

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// Backend hospedado no Render.com — https://horta-back.onrender.com
 export const api = axios.create({
-  baseURL: "https://horta-back.onrender.com", // 🔥 URL DO BACKEND
+  baseURL: "https://horta-back.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
